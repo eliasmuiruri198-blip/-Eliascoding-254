5fdf3affc80d683b7415379b6fa3aac8c57ea262
eliasmuiruri198-blip <eliasmuiruri198@gmail.com>
2025-09-17 22:58:50 +0300

    Add files via upload

---
+++ Programming Assignment Question I.c
@@ -0,0 +1,19 @@
+//C program 
+/*
+Name: Elias
+Reg: CT100/G/26169/25
+Description:Semester 1 units 
+*/
+#include<stdio.h>
+
+//main function 
+int main(){
+printf("Computer systems and organization");
+printf ("Fundamentals of ICT");
+printf ("Essentials of Accounting");
+printf ("Analogue Electronics");
+printf ("Introduction to computer application");
+printf ("Communication skills");
+printf ("Mathematics for Science");
+printf ("Introduction to programming and Algorithms");
+}
---
+++ Programming Assignment Question II.c
@@ -0,0 +1,22 @@
+//C program 
+/*
+Name: Elias
+Reg: CT100/G/26169/25
+Description:To prompt the student to
+enter height,weight,phonenumber 
+*/
+
+#include<stdio.h>
+int main(){
+int height ,weight,phone;
+    printf("Enter your height");
+    scanf("%d",&height);
+    printf ("Enter your weight");
+    scanf("%d",&weight);
+    printf ("Enter your phone");
+    scanf("%d",&phone);
+    printf ("height ;%d",height);
+    printf("weight ;%d",weight);
+    printf("phone ;%d",phone);
+printf( "weight;%d",weight);
+}
