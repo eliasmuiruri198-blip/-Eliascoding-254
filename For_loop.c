bec735317fb711d1e52975508b6b152042979bae
eliasmuiruri198-blip <eliasmuiruri198@gmail.com>
2025-10-07 06:51:16 +0300

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,20 @@
+/*
+Name:Muiruri Elias 
+Reg no:CT100/G/26169/25
+Description :program to write numbers to check out the use of for loop
+*/
+#include<stdio.h>
+
+//main function 
+int main(){
+
+//for loop(start;stop;step);
+
+int i;
+for(i=100;i>=50;i--){
+
+printf ("%d \n",i);
+}
+return 0;
+
+}
